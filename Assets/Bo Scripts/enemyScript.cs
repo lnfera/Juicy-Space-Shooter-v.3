dff1cfb0210f8e3efc1fd1a391240bdf099b0c65
W0rdS0up <79453957+W0rdS0up@users.noreply.github.com>
2021-10-14 13:32:21 +0200

    minor changes

---
+++ Assets/Bo Scripts/enemyScript.cs
@@ -0,0 +1,43 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public Transform tr;
+    public int hp;
+    public int pointValue; // hur mycket points till scoren men får av att död enemyn 
+    public float speed;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(new Vector2(tr.position.x, tr.position.y - speed));
+    }
+    void takeDamage(int damage) // metod för när enemyn tar skada 
+    {
+        hp -= damage;
+        if(hp <= 0)
+        {
+            death();
+        }
+    }
+    void death() // metod för när enemyn dör 
+    {
+        
+
+    }
+}
