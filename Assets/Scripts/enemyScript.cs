5ba9cdd4ea8b679108312a6c7222bbdceb69a6e6
W0rdS0up <79453957+W0rdS0up@users.noreply.github.com>
2021-10-14 10:51:06 +0200

    enemy script
    start på enemy script

---
+++ Assets/Scripts/enemyScript.cs
@@ -0,0 +1,23 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rd;
+    public int hp;
+    public int pointValue;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
