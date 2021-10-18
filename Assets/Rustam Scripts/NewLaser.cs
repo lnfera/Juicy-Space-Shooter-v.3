26d8c613bcd3452311b2dbd96517fe3e977a7c8b
RustamLBS <rustam.nurgushev@elev.ga.lbs.se>
2021-10-18 13:43:04 +0200

    Importering av Vapen

---
+++ Assets/Rustam Scripts/NewLaser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewLaser : NewWeapons
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+    public override void Fire()
+    {
+        base.Fire();
+        print("Bzzzzt");
+        Rigidbody2D instantiatedProjectile = Instantiate(LaserBody, transform.position, Quaternion.Euler(0, 0, 0));
+        instantiatedProjectile.AddForce(transform.up * 500);
+        print("Brrr bap");
+    }
+}
---
+++ Assets/Rustam Scripts/NewSMG.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSMG : NewWeapons
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+    public override void Fire()
+    {
+        base.Fire();
+        Rigidbody2D instantiatedProjectile = Instantiate(BulletBody, transform.position, Quaternion.Euler(0, 0, 0));
+        instantiatedProjectile.AddForce(transform.up * 1500);
+        print("Brrr bap");
+    }
+}
