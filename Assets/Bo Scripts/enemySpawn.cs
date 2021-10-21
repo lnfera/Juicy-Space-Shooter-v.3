de51dbd6c27077e338f39501e670f1b3c14e2efd
W0rdS0up <79453957+W0rdS0up@users.noreply.github.com>
2021-10-21 10:40:49 +0200

    enemy spawn grejor

--- Assets/Bo Scripts/enemySpawn.cs
+++ Assets/Bo Scripts/enemySpawn.cs
@@ -6,12 +6,28 @@ public class enemySpawn : MonoBehaviour
 {
     public GameObject[] prefabsEnemy;
     public int wave;
-    public int waveDifficultyScore;
+    public int waveSpawnScore;
+    int selctedEnemy;
+    public float timeBetweenSpawns;
+    public float cdSpawn; // cooldown mellan enemy spans 
+    public 
 
 
     void Start()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        cdSpawn -= Time.fixedDeltaTime;
+        if (cdSpawn < 0)
+        {
+            spawnEnemy();
+        }
+        if (waveSpawnScore <= 0)
+        {
+            NewWave();
+        }
     }
 
 
@@ -19,9 +35,27 @@ public class enemySpawn : MonoBehaviour
     {
         
     }
-    public void onNewWave()
+    public void NewWave()
     {
         wave += 1;
-        waveDifficultyScore = wave * 10 + 10;
+        waveSpawnScore = wave * 10 + 10;
+        cdSpawn = 5;
+        print("new wave " + wave);
+    }
+    public void spawnEnemy()
+    {
+        cdSpawn = timeBetweenSpawns;
+        if (waveSpawnScore <= 15)
+        {
+            selctedEnemy = Random.Range(1, waveSpawnScore);
+        }
+        else
+        {
+            selctedEnemy = Random.Range(1, prefabsEnemy.Length - 1);
+        }
+        print("selctedEnemy" + selctedEnemy);
+        waveSpawnScore -= selctedEnemy;
+
+        Instantiate(prefabsEnemy[selctedEnemy -1], new Vector3(0, 0, 0), Quaternion.identity);
     }
 }
