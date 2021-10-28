dfc49bd9ab8e5bef23cfb19441cd822523cc9610
RustamLBS <rustam.nurgushev@elev.ga.lbs.se>
2021-10-28 09:47:11 +0200

    Weapon Rebalance

--- Assets/Rustam Scripts/NewSMG.cs
+++ Assets/Rustam Scripts/NewSMG.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewSMG : NewWeapons
 {
+    private bool StopShoot = false;
     //Skrivet av Rustam  
     // Start is called before the first frame update
     void Start()
@@ -14,17 +15,33 @@ public class NewSMG : NewWeapons
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            Fire();
-            //När man trycker på space så påbörjar skjutfunktionen
+            if (StopShoot == false)
+            {
+                Fire();
+            }
+            //När man håller ner space så påbörjar skjutfunktionen och man skjuter automatiskt
         }
     }
     public override void Fire()
     {
         base.Fire();
+        StartCoroutine(FirePause());
+
+    }
+    IEnumerator FirePause()
+    {
+        //Cooldownen startas
+        StopShoot = true;
+        yield return null;
+
+        //En kula spawnar och skickas med en kraft uppåt.
         Rigidbody2D instantiatedProjectile = Instantiate(BulletBody, transform.position, Quaternion.Euler(0, 0, 0));
         instantiatedProjectile.AddForce(transform.up * 1500);
-        //En kula spawnar och skickas med en kraft uppåt.
+
+        //0.25 Sekunder senare slutar cooldownen
+        yield return new WaitForSeconds(0.25f);
+        StopShoot = false;
     }
 }
